32c156fb1902d43fed36152ff9b45795fd291eef
jacquelynlaw <jacquelynlaw@gmail.com>
2019-10-25 14:40:46 -0400

    Created class for rule06 compliant code

---
+++ rule06.java
@@ -0,0 +1,12 @@
+
+/**
+ * Unspecified behavior and losing data:
+ *  Arises when we encode noncharacter data as a string
+ * Prevention:
+ *  Don't encode noncharacter data as a string
+ */
+public class rule06 {
+    public static void main(String args[]) {
+        
+    }
+}
