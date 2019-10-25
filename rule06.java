610de3c465025389c4d12328144f32b5c3afe74f
jacquelynlaw <jacquelynlaw@gmail.com>
2019-10-25 14:49:19 -0400

    Added rule06 compliant code that does not use assertions to validate method arguments

--- rule06.java
+++ rule06.java
@@ -1,12 +1,29 @@
-
 /**
- * Unspecified behavior and losing data:
- *  Arises when we encode noncharacter data as a string
+ * Incorrect results:
+ *  Arises we use assertions to validate method arguments
  * Prevention:
- *  Don't encode noncharacter data as a string
+ *  Check for illegal arguments
  */
 public class rule06 {
     public static void main(String args[]) {
-        
+        // Add absolute values of integers
+        // Case that passes
+        System.out.println(getAbsAdd(1, 0));
+        // Case that throws exception
+        System.out.println(getAbsAdd(Integer.MAX_VALUE, 33333333));
+    }
+
+    public static int getAbsAdd(int x, int y) {
+        if (x == Integer.MIN_VALUE || y == Integer.MIN_VALUE) {
+            throw new IllegalArgumentException();
+        }
+
+        int absX = Math.abs(x);
+        int absY = Math.abs(y);
+        if (absX > Integer.MAX_VALUE - absY) {
+            throw new IllegalArgumentException();
+        }
+
+        return absX + absY;
     }
 }
