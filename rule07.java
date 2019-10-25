db9a3d84cadf5ab352ea9ff14e354d2adee2db63
jacquelynlaw <jacquelynlaw@gmail.com>
2019-10-25 17:09:19 -0400

    Added compliant code for rule07, do not ignore/suppress exceptions

---
+++ rule07.java
@@ -0,0 +1,26 @@
+
+
+/**
+ * ERR00-J. Do not suppress or ignore checked exceptions
+ * Exception is not propagated forward:
+ *  Arises when we ignore exceptions in trivial try catch
+ * Prevention:
+ *  Allow exception to propagate outwards
+ */
+public class rule07 {
+    public static void main(String args[]) {
+        Foo fooSample = new Foo();
+        fooSample.run();
+    }
+}
+
+class Foo implements Runnable {
+    public void run() {
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException e) {
+        System.err.println(e);
+        Thread.currentThread().interrupt(); // Reset interrupted status
+      }
+    }
+  }
