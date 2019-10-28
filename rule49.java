45b3ec6cacc3ac20da6d9afabf821e67e0c6a504
jacquelynlaw <jacquelynlaw@gmail.com>
2019-10-28 11:56:29 -0400

    Wrote compliant code to securely generate true random integers in Java

--- rule49.java
+++ rule49.java
@@ -1,12 +1,23 @@
+import java.security.SecureRandom;
+import java.security.NoSuchAlgorithmException;
 /**
- * MSC02-J. Do not suppress or ignore checked exceptions
- * Exception is not propagated forward:
- *  Arises when we ignore exceptions in trivial try catch
+ * MSC02-J. Generate strong random numbers
+ * Psuedorandom number generators do not achive true randomness:
+ *  Arises when we use java.util.Random (a PRNG)
+ *  Attacker can learn value of seed value
  * Prevention:
- *  Allow exception to propagate outwards
+ *  Use a more secure random number generator like java.security.SecureRandom
  */
 public class rule49 {
     public static void main(String args[]) {
-
+        try {
+            SecureRandom number = SecureRandom.getInstanceStrong();
+            // Generate 5 random integers between 0..100
+            for (int i = 0; i < 5; i++) {
+                System.out.println(number.nextInt(101));
+            }
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println(e);
+        }
     }
 }
