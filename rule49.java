9a224e41643598d907388b3408ca17cbf74ef6db
jacquelynlaw <jacquelynlaw@gmail.com>
2019-10-28 11:50:54 -0400

    Created class for new rule

---
+++ rule49.java
@@ -0,0 +1,12 @@
+/**
+ * MSC02-J. Do not suppress or ignore checked exceptions
+ * Exception is not propagated forward:
+ *  Arises when we ignore exceptions in trivial try catch
+ * Prevention:
+ *  Allow exception to propagate outwards
+ */
+public class rule49 {
+    public static void main(String args[]) {
+
+    }
+}
